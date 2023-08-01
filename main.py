765d2d4f2c4f39f864c131d9843e344c63c40bd4
mp768 <60555816+mp768@users.noreply.github.com>
2023-07-31 23:06:42 -0500

    good enough

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ from util.objects import *
 from util.routines import *
 from util.tools import find_hits
 
+# import enum for bot mode
+from enum import Enum
+import math
+
 import time
+
+class BotMode(Enum):
+    Defend = 0
+    Attack = 1
+    GatherBoost = 2
+    Kickoff = 3
+
+def clamp(val, min, max):
+    return min if val < min else max if val > max else val
 
 class Bot(GoslingAgent):
     debug_text = ''
@@ -17,6 +30,17 @@ class Bot(GoslingAgent):
         white = self.renderer.white()
         self.renderer.draw_string_2d(10, 150, 3, 3, self.debug_text, white)
     
+    def set_good_jump_shot(self, target):
+        orientation = Vector3(0, 0, 0)
+        
+        for o in [self.me.forward, self.me.left]:
+            orientation += o.normalize()
+        
+        orientation = orientation
+        
+        # orientation = (self.me.forward + self.me.left).normalize()
+        self.set_intent(jump_shot(self.ball.location, self.time, (target - self.ball.location).normalize(), orientation))
+        
     def run(self):
         self.last_time = self.current_time
         self.current_time = time.time()
@@ -27,7 +51,7 @@ class Bot(GoslingAgent):
         
         if self.intent is not None:
             if type(self.intent) == goto_boost and self.timer > 75:
-                self.set_intent(goto(self.ball.location))
+                self.intent = None
                 self.timer = 0
             
             return
@@ -36,35 +60,41 @@ class Bot(GoslingAgent):
             self.set_intent(kickoff())
             return
 
+        # self.set_good_jump_shot()
+
         if self.is_in_front_of_ball():
             # self.set_intent(short_shot(self.foe_goal.location))
-            self.set_intent(jump_shot(self.ball.location, self.time, (self.ball.location - self.foe_goal.location).normalize(), self.me.orientation.forward.normalize()))
+            self.set_good_jump_shot(self.foe_goal.location)
             return
         
         if (self.foe_goal.location - self.ball.location).magnitude() < 1000:
-            self.set_intent(goto(self.ball.location))
+            self.set_good_jump_shot(self.foe_goal.location)
+            # self.set_intent(goto(self.ball.location))
             return
         
-        if (self.friend_goal.location - self.me.location).magnitude() < 500 and self.went_to_goal:
-            self.set_intent(goto(self.ball.location))
+        if (self.friend_goal.location - self.me.location).magnitude() < 500:
+            b = self.get_closest_boost(False)
+            self.set_good_jump_shot(b.location if b is not None else self.ball.location)
+            # self.set_intent(goto(self.ball.location))
             return
         
-        if (self.friend_goal.location - self.ball.location).magnitude() < 1000 and not self.went_to_goal:
-            self.set_intent(goto(self.friend_goal.location))
-            self.went_to_goal = True
-            return
+        # if (self.friend_goal.location - self.ball.location).magnitude() < 1000:
+        #     self.set_intent(goto(self.friend_goal.location))
+        #     self.went_to_goal = True
+        #     return
         
         foe = self.get_closest_foe()
         
-        if self.me.boost > 50 and foe is not None:
-            self.set_intent(ram_with_boost(foe))
-            print("Found a foe to ram ", foe)
-            print(self.foes)
-            self.went_to_goal = False
-            return
+        if self.me.boost > 75 and foe is not None and not self.is_in_front_of_ball():
+            if (foe.location - self.me.location).magnitude() < 500:
+                self.set_intent(ram_with_boost(foe))
+                print("Found a foe to ram ", foe)
+                print(self.foes)
+                self.went_to_goal = False
+                return
         
         if foe is not None and (foe.location - self.foe_goal.location).magnitude() > 100:
-            # self.set_intent(jump_shot(self.ball.location, 1, (self.foe_goal.location - self.ball.location).normalize(), self.me.orientation.forward.normalize()))
+            # self.set_good_jump_shot()
             self.set_intent(short_shot(self.foe_goal.location))
             return
         
@@ -85,76 +115,6 @@ class Bot(GoslingAgent):
             return
         
         self.set_intent(jump_shot(self.ball.location, self.time, (self.ball.location - self.foe_goal.location).normalize(), self.me.orientation.forward.normalize()))
-        
-        
-    
-    # This function runs every in-game tick (every time the game updates anything)
-    # def run(self):
-    #     self.debug_print()
-        
-    #     self.last_time = self.current_time
-    #     self.current_time = time.time()
-        
-    #     delta_time = self.current_time - self.last_time
-        
-    #     self.timer += 10 * delta_time
-        
-    #     if self.get_intent() is not None:
-    #         if type(self.intent) == goto_boost and self.timer > 100:
-    #             self.set_intent(goto(self.ball.location))
-    #             self.timer = 0
-            
-    #         self.debug_intent()
-    #         return
-        
-    #     # set_intent tells the bot what it's trying to do
-    #     if self.kickoff_flag:
-    #         self.set_intent(kickoff())
-    #         return
-    
-    #     if self.is_in_front_of_ball():
-    #         self.set_intent(short_shot(self.foe_goal.location))
-    #         self.debug_text = "Retreating"
-    #         return
-        
-    #     if self.me.boost > 95:
-    #         foe = self.get_closest_foe()
-    #         if foe is not None:
-    #             self.set_intent(ram_with_boost(foe))
-    #         return
-        
-    #     if self.me.boost > 75:
-    #         self.set_intent(short_shot(self.foe_goal.location))
-    #         self.debug_text = "Shooting the ball"
-    #         return
-        
-    #     if (self.friend_goal.location - self.ball.location).magnitude() < 800:
-    #         self.set_intent(goto(self.friend_goal.location))
-    #         return
-        
-    #     if (self.me.location - self.ball.location).magnitude() < 1000:
-    #         self.set_intent(short_shot(self.foe_goal.location))
-    #         # self.set_intent(short_shot(self.foe_goal.location))
-    #         return
-        
-    #     if self.me.boost > 5:
-    #         self.set_intent(goto(self.ball.location))
-    #         closest_boost = self.get_closest_boost(True)
-            
-    #         if closest_boost is not None:
-    #             self.set_intent(goto_boost(closest_boost))
-            
-    #         self.debug_text = "Getting boost"
-    #         return
-        
-    #     closest_boost = self.get_closest_boost(True)
-        
-    #     if closest_boost is not None:
-    #         self.set_intent(goto_boost(closest_boost))
-    #         self.debug_text = "Getting boost"
-    #         return
-        
-    #     self.set_intent(recovery())
     
     def is_in_front_of_ball(self):
         me_to_goal = (self.me.location - self.foe_goal.location).magnitude()
